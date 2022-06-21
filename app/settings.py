0a4693a22defc1cdeee1662b8a6b053176b2ca19
HenryDani <lagunasdaniel001@gmail.com>
2022-06-21 00:14:03 -0500

    Configuracion heroku 4.0

--- app/settings.py
+++ app/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR,'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'bases:login'
