4487bab518aaf5133d2cdeebea10480352944b75
HenryDani <lagunasdaniel001@gmail.com>
2022-06-21 01:09:37 -0500

    dd

--- bases/urls.py
+++ bases/urls.py
@@ -3,7 +3,7 @@ from .views import Home, HomeSinPrivilegios
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('login/', LoginView.as_view(template_name='bases/login.html'), name='login'),
+    path('favicon.ico/', LoginView.as_view(template_name='bases/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('sin_privilegios/', HomeSinPrivilegios.as_view(), name='sin_privilegios'),
 ]
