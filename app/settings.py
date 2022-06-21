30dcd3b8621339d544681546766b1eaef6d09b7c
HenryDani <lagunasdaniel001@gmail.com>
2022-06-21 18:12:48 -0500

    ddd

--- app/settings.py
+++ app/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'fac',
     'django_userforeignkey',
     'rest_framework',
-    'api',
+    # 'api',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': (BASE_DIR,'templates'),
+        'DIRS': [os.path.join(BASE_DIR,'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
