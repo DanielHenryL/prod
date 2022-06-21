d89a666104359a1905a3c1d5e4a0db21be4c5b8d
HenryDani <lagunasdaniel001@gmail.com>
2022-06-21 00:05:31 -0500

    comentario

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', include(('bases.urls','bases'), namespace='bases')),
     path('inv/', include(('inv.urls','inv'), namespace='inv')),
@@ -8,4 +9,4 @@ urlpatterns = [
     path('fac/', include(('fac.urls','fac'), namespace='fac')),
     path('api/', include(('api.urls','api'), namespace='api')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
