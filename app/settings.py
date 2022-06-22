bc7c0218ef01faf97ed95d36eb891208516081f6
HenryDani <lagunasdaniel001@gmail.com>
2022-06-21 22:00:06 -0500

    Con fe

--- app/settings.py
+++ app/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static/base'),)
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
